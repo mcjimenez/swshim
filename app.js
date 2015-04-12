ceb4bf7edff0608811b796d013266041ca20c4b8
Carmen Jimenez Cabezas <cjc@tid.es>
2015-04-12 16:29:12 +0200

    wip

--- app.js
+++ app.js
@@ -12,24 +12,39 @@
     debug('navigator has not ServiceWorker');
     return;
   }
+
+  var register = function(evt) {
+    debug('executing register...');
+    navigator.serviceWorker.register('sw.js', {scope: '/swshim/'}
+    ).then(function(reg) {
+      debug('Registration succeeded. Scope: ' + reg.scope);
+      if (reg.installing) {
+        debug('registration --> installing');
+      } else if (reg.waiting) {
+        debug('registration --> waiting');
+      } else if (reg.active) {
+        debug('registration --> active');
+      }
+    }).catch(function(error) {
+      debug('Registration failed with ' + error);
+    });
+  };
+
+  var unregister = function(evt) {
+    debug('Unregister...');
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      registrations.forEach(function (registration) {
+        registration.unregister();
+        debug('Unregister done');
+      });
+    });
+  };
+
   window.addEventListener('load', function () {
     debug("Document loaded!");
     var regBto = document.querySelector('#regBto');
-    regBto.addEventListener('click', function(evt) {
-      debug('executing register...');
-      navigator.serviceWorker.register('sw.js', {scope: '/swshim/'}
-        ).then(function(reg) {
-          debug('Registration succeeded. Scope: ' + reg.scope);
-            if (reg.installing) {
-            debug('registration --> installing');
-            } else if (reg.waiting) {
-            debug('registration --> waiting');
-            } else if (reg.active) {
-            debug('registration --> active');
-            }
-        }).catch(function(error) {
-          debug('Registration failed with ' + error);
-        });
-    });
+    var unRegBto = document.querySelector('#unregBto');
+    regBto.addEventListener('click', register);
+    unRegBto.addEventListener('click', unregister);
   });
 })();
